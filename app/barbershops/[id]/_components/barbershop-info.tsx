336084e4ce84beed2686ba871e59abfc1656003c
dsaBruno <bruuno.dsa@gmail.com>
2024-05-18 17:12:14 -0300

    add: ensure unauthenticated user is redirected to login on booking click

--- app/barbershops/[id]/_components/barbershop-info.tsx
+++ app/barbershops/[id]/_components/barbershop-info.tsx
@@ -1,56 +1,67 @@
 "use client"
 
+import SideMenu from "@/app/_components/side-menu";
 import { Button } from "@/app/_components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/app/_components/ui/sheet";
 import { Barbershop } from "@prisma/client";
-import { ChevronLeftIcon, MenuIcon, MapPinIcon, StarIcon } from "lucide-react";
+import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 interface BarbershopInfoProps {
-  barbershop: Barbershop
+  barbershop: Barbershop;
 }
 
-const BarbershopInfo = ({barbershop}: BarbershopInfoProps) => {
-  const router = useRouter()
+const BarbershopInfo = ({ barbershop }: BarbershopInfoProps) => {
+  const router = useRouter();
 
   const handleBackClick = () => {
-    router.back()
-  }
+    router.replace("/");
+  };
 
   return (
-  <div>
-    <div className="h-[250px] w-full relative">
-      <Button onClick={handleBackClick} size="icon" variant="outline" className="z-50 absolute top-4 left-4">
-        <ChevronLeftIcon />
-      </Button>
+    <div>
+      <div className="h-[250px] w-full relative">
+        <Button onClick={handleBackClick} size="icon" variant="outline" className="z-50 absolute top-4 left-4">
+          <ChevronLeftIcon />
+        </Button>
 
-      <Button size="icon" variant="outline" className="z-50 absolute top-4 right-4">
-        <MenuIcon />
-      </Button>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button size="icon" variant="outline" className="z-50 absolute top-4 right-4">
+              <MenuIcon />
+            </Button>
+          </SheetTrigger>
 
-        <Image 
-          src={barbershop.imageUrl} 
-          alt={barbershop.name} 
-          fill 
+          <SheetContent className="p-0">
+            <SideMenu />
+          </SheetContent>
+        </Sheet>
+
+        <Image
+          src={barbershop.imageUrl}
+          fill
+          alt={barbershop.name}
           style={{
-            objectFit: "cover"
-            }} 
-            className="opacity-75"
+            objectFit: "cover",
+          }}
+          className="opacity-75"
         />
-    </div>
+      </div>
 
-    <div className="px-5 pt-3 pb-6 border-b border-solid border-secondary">
-      <h1 className="text-xl font-bold">{barbershop.name}</h1>
-      <div className="flex items-center gap-1 mt-2">
-        <MapPinIcon className="text-primary" size={18}/>
-        <p className="text-sm">{barbershop.address}</p>
-      </div>
-      <div className="flex items-center gap-1 mt-2">
-        <StarIcon className="text-primary" size={18}/>
-        <p className="text-sm">4,8 (899 avaliações)</p>
+      <div className="px-5 pt-3 pb-6 border-b border-solid border-secondary">
+        <h1 className="text-xl font-bold">{barbershop.name}</h1>
+        <div className="flex items-center gap-1 mt-2">
+          <MapPinIcon className="text-primary" size={18} />
+          <p className="text-sm">{barbershop.address}</p>
+        </div>
+        <div className="flex items-center gap-1 mt-2">
+          <StarIcon className="text-primary" size={18} />
+          <p className="text-sm">5,0 (899 avaliações)</p>
+        </div>
       </div>
     </div>
-  </div>   );
-}
- 
+  );
+};
+
 export default BarbershopInfo;
--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -1,33 +1,35 @@
 import { db } from "@/app/_lib/prisma";
-import Image from "next/image";
-import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react";
 import BarbershopInfo from "./_components/barbershop-info";
 import ServiceItem from "./_components/service-item";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 interface BarbershopDetailsPageProps {
   params: {
     id?: string;
-  }
+  };
 }
 
+const BarbershopDetailsPage = async ({ params }: BarbershopDetailsPageProps) => {
+  const session = await getServerSession(authOptions);
 
-const BarbershopDetailsPage = async ({params}: BarbershopDetailsPageProps) => {
   if (!params.id) {
-    //TODO redirect to 404
-    return null
+    // TODO: redirecionar para home page
+    return null;
   }
 
   const barbershop = await db.barbershop.findUnique({
     where: {
-      id: params.id
+      id: params.id,
     },
     include: {
-      services: true
-    }
-  })
+      services: true,
+    },
+  });
 
   if (!barbershop) {
-    return null
+    // TODO: redirecionar para home page
+    return null;
   }
 
   return (
@@ -36,11 +38,11 @@ const BarbershopDetailsPage = async ({params}: BarbershopDetailsPageProps) => {
 
       <div className="px-5 flex flex-col gap-4 py-6">
         {barbershop.services.map((service) => (
-          <ServiceItem key={service.id} service={service} />
+          <ServiceItem key={service.id} service={service} isAuthenticated={!!session?.user} />
         ))}
       </div>
     </div>
-  )
+  );
 };
- 
+
 export default BarbershopDetailsPage;
